1db8fe83d1c01f5acd1c854a36cbb6c170666b85
RockersAj23 <mulgir.ajay.che22@iitbhu.ac.in>
2024-07-30 23:36:53 +0530

    first kaggle done

---
+++ cp_practice.cpp
@@ -0,0 +1,22 @@
+//                                                     हरि ॐ तत्सत्
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+#define ll long long
+#define pyes cout << "Yes" << endl;
+#define pno cout << "No" << endl;
+#define checkp cout << "AJAY" << endl;
+#define nln cout << endl;
+#define show(a)                       \
+    rep(i, 0, n) cout << a[i] << " "; \
+    nln;
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define acc(x) accumulate(all(x), (ll)0)
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ll t;
+    cin >> t;
+    
+}
